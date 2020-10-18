e5690743ebd6c86316c37a10ec64ffd7055c17d2
nobel <nobelinderpreet@gmail.com>
2020-10-18 19:13:53 +1100

    Updates

---
+++ precache-manifest.f3fed091b249cf6b5caef0fc4ddb2016.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a0191c8a0c717b2bc9427ca0a9f0113",
+    "url": "/Reactjs-travel-website/index.html"
+  },
+  {
+    "revision": "3bc821483b70f394f45a",
+    "url": "/Reactjs-travel-website/static/css/2.e6ad0f51.chunk.css"
+  },
+  {
+    "revision": "2b3508ff17144aa55fa6",
+    "url": "/Reactjs-travel-website/static/css/main.b3420024.chunk.css"
+  },
+  {
+    "revision": "3bc821483b70f394f45a",
+    "url": "/Reactjs-travel-website/static/js/2.951fbf39.chunk.js"
+  },
+  {
+    "revision": "706ae2ba09b6b4555454ecd6058d6910",
+    "url": "/Reactjs-travel-website/static/js/2.951fbf39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b3508ff17144aa55fa6",
+    "url": "/Reactjs-travel-website/static/js/main.938cf4b2.chunk.js"
+  },
+  {
+    "revision": "fb573ff23a2c4310f343",
+    "url": "/Reactjs-travel-website/static/js/runtime-main.13de2cde.js"
+  },
+  {
+    "revision": "03b704faf7e943dbfe5aaaac1ef4a121",
+    "url": "/Reactjs-travel-website/static/media/3.03b704fa.jpeg"
+  },
+  {
+    "revision": "7372b24d0a84d72a963136a581d4d7ce",
+    "url": "/Reactjs-travel-website/static/media/4.7372b24d.jpeg"
+  },
+  {
+    "revision": "b707b22bc26a7502417ca34af6fe420a",
+    "url": "/Reactjs-travel-website/static/media/Destination.b707b22b.jpeg"
+  },
+  {
+    "revision": "ff72acfd489d571ed7c14177692e1206",
+    "url": "/Reactjs-travel-website/static/media/chef2.ff72acfd.jpeg"
+  },
+  {
+    "revision": "60aa6678bd60d673300b10a6707552ff",
+    "url": "/Reactjs-travel-website/static/media/deals.60aa6678.jpeg"
+  },
+  {
+    "revision": "2738cc899742ba483fceb3152e2e187b",
+    "url": "/Reactjs-travel-website/static/media/girl.2738cc89.jpeg"
+  },
+  {
+    "revision": "48cd9538e4d4d31f12c6e0d54039e491",
+    "url": "/Reactjs-travel-website/static/media/hotels.48cd9538.jpeg"
+  },
+  {
+    "revision": "5575033f82030cb4ec7604e36778163b",
+    "url": "/Reactjs-travel-website/static/media/suitcase.5575033f.jpeg"
+  },
+  {
+    "revision": "4cf816c70b9e676ca47efbb2610c478a",
+    "url": "/Reactjs-travel-website/static/media/travel.4cf816c7.jpeg"
+  }
+]);
